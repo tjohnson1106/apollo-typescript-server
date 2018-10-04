38aac846484f9ff4aa208a2ce5f7b03bcee2cd9d
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-04 14:51:04 -0400

    created register user mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,21 @@
-export const resolvers = {
+import { IResolvers } from "graphql-tools";
+import * as bcrypt from "bcryptjs";
+
+import { User } from "./entity/User";
+
+export const resolvers: IResolvers = {
   Query: {
     hello: () => "Hello word"
+  },
+  Mutation: {
+    register: async (_, { email, password }) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await User.create({
+        email,
+        password: hashedPassword
+      }).save();
+
+      return true;
+    }
   }
 };
