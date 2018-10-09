7cfbd0e3bfda1de30ba6678d8f73dfdd6215e7d6
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-09 14:16:49 -0400

    created returning customer capability in create subscription resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -52,13 +52,33 @@ export const resolvers: IResolvers = {
         throw new Error();
       }
 
-      const customer = await stripe.customers.create({
-        email: user.email,
-        source,
-        plan: process.env.PLAN
-      });
+      let stripeId = user.stripeId;
 
-      user.stripeId = customer.id;
+      if (!stripeId) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          source,
+          plan: process.env.PLAN
+        });
+        stripeId = customer.id;
+      } else {
+        // update customer
+
+        await stripe.customers.update(stripeId, {
+          source
+        });
+
+        await stripe.subscriptions.create({
+          customer: stripeId,
+          items: [
+            {
+              plan: process.env.PLAN!
+            }
+          ]
+        });
+      }
+
+      user.stripeId = stripeId;
       user.type = "paid";
       user.ccLast4 = ccLast4;
       await user.save();
